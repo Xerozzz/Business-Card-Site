8bd67e106e186eae90ebf85376de9a16ddc5bd07
Xerozzz <60227509+Xerozzz@users.noreply.github.com>
2024-06-06 22:45:51 +0800

    Edit styling

--- Frontend/src/components/profiles/Details.js
+++ Frontend/src/components/profiles/Details.js
@@ -1,7 +1,7 @@
 export default function Details({ name, role, company }) {
   return (
     <div className="details">
-      <h1>{name}</h1>
+      <h1 className="companyrole">{name}</h1>
       <h2>{role}</h2>
       <h2>{company}</h2>
     </div>
